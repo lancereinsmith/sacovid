dffbccbbde0cc72e5cf9fc0ba4d41853be0bd636
Lance Reinsmith <lancereinsmith@gmail.com>
2020-06-20 17:58:28 -0500

    new changes

--- covid_dash.py
+++ covid_dash.py
@@ -26,65 +26,80 @@ df = df[df.index.notnull()]
 for col in ['Recovered', 'Hospitalized']:
     df[col + "_Daily"] = (df[col] - df[col].shift(1)).dropna()
 
-for col in ['COVIDnICU', 'COVIDonVent']:
-    df[col + "_Cum"] = df[col].cumsum()
-
 ## Create dict for chart options
 chart_dict = {
+    "Multiview": ("Multiview Chart"),
     "Reported Cases": ("Cumulative and Daily Reported Cases", "ReportedCum", "ReportedOn"),
     "Testing Information": ("Cumulative and Daily Testing Information", None, None),
     "Mortality": ("Cumulative and Daily Mortality Information", "DeathsCum", "Deceased"),
     "Recoveries": ("Cumulative and Daily Recovery Information", "Recovered", "Recovered_Daily"),
     "Hospitalizations": ("Cumulative and Daily Hospitalization Information", "Hospitalized", "Hospitalized_Daily"),
-    "COVID ICU Patients": ("Cumulative and Daily COVID ICU Patients", "COVIDnICU_Cum", "COVIDnICU"),
-    "COVID Ventilator Patients": ("Cumulative and Daily COVID Ventilator Patients", "COVIDonVent_Cum", "COVIDonVent"),
+    "COVID ICU Patients": ("Daily COVID ICU Patients", "COVIDnICU"),
+    "COVID Ventilator Patients": ("Daily COVID Ventilator Patients", "COVIDonVent"),
     "Quarantine": ("Cumulative and Weekly Quarantine Information", "EverQuar", "WeekQuar"),
     "Ventilator Availability": ("Ventilator Availability Information", "TotalVents", "AvailVent", "Ventilators"),
     "Staffed Bed Availability": ("Staffed Bed Availability Information", "TotalStaffedBeds", "AvailStaffedBeds", "Staffed Beds")
 }
 
 ## Sidebar
-if st.sidebar.checkbox("See source data"):
-    st.write("Source data:")
-    st.write(URL)
-    st.dataframe(df)
-
 start_date = st.sidebar.date_input("Start Date", value=datetime(2020, 3, 19))
 end_date = st.sidebar.date_input("End Date", value=df.index.max())
 
-options_list = ["Reported Cases",
-                "Testing Information",
-                "Mortality",
-                "Recoveries",
-                "Hospitalizations",
-                "COVID ICU Patients",
-                "COVID Ventilator Patients",
-                "Quarantine",
-                "Ventilator Availability",
-                "Staffed Bed Availability"
-                ]
+multi_options = {"Cumulative Reported Cases": "ReportedCum",
+                "Daily Reported Cases": "ReportedOn",
+                "Cumulative Mortality": "DeathsCum",
+                "Daily Mortality": "Deceased",
+                "Cumulative Recoveries": "Recovered",
+                "Daily Recoveries": "Recovered_Daily",
+                "Cumulative Hospitalizations": "Hospitalized",
+                "Daily Hospitalizations": "Hospitalized_Daily",
+                "Daily COVID ICU Patients": "COVIDnICU",
+                "Daily COVID Ventilator Patients":"COVIDonVent"
+}
 
 choices = st.sidebar.multiselect("Select individual charts to display:",
-                        options=options_list,
-                        default=options_list[:2])
+                        options=list(chart_dict.keys()),
+                        default=list(chart_dict.keys())[:3])
 
 st.sidebar.markdown('### HELP:\n* Enter your start and stop dates.\n* Click the magnifying glass to select charts to display.  Or, type a keyword in the search bar.')
 
+
+
 ## Render the main screen
-st.markdown("# San Antonio COVID-19 Charts")
-st.markdown(f'#### The data for these charts were last updated at {df.index.max()}. Source:')
+st.title("San Antonio COVID-19 Charts")
+st.subheader(f'The data for these charts were last updated at {df.index.max()}. Source:')
 st.code(URL)
 
+if st.checkbox("See source data"):
+    st.write("Source data:")
+    st.dataframe(df)
+
 for choice in choices:
-    if choice in ["Reported Cases", "Mortality", "Recoveries", "Hospitalizations", "COVID ICU Patients", "COVID Ventilator Patients"]:
-        st.markdown("## "+chart_dict[choice][0])
+    if choice in ["Multiview"]:
+        multi_choice = st.multiselect("Select charts to display:",
+                        options=list(multi_options.keys()),
+                        default=list(multi_options.keys())[0])
+        if len(multi_choice) > 0:
+            ax = df[[multi_options[x] for x in multi_choice] ].loc[start_date : end_date + timedelta(days=1)].plot(title = "Multiview Chart")
+            ax.legend(multi_choice);
+            st.pyplot()
+        else:
+            st.subheader("Please select one or more charts to display.")
+
+    elif choice in ["Reported Cases", "Mortality", "Recoveries", "Hospitalizations"]:
+        st.header(chart_dict[choice][0])
         df[chart_dict[choice][1]].loc[start_date : end_date + timedelta(days=1)].plot(title = "Cumulative " + choice)
         st.pyplot()
         ax = df[chart_dict[choice][2]].loc[start_date : end_date + timedelta(days=1)].plot(title = "Daily " + choice, c='r')
         st.pyplot()
 
+    elif choice in ["COVID ICU Patients", "COVID Ventilator Patients"]:
+        st.header(chart_dict[choice][0])
+        df[chart_dict[choice][1]].loc[start_date : end_date + timedelta(days=1)].plot(title = "Daily " + choice)
+        st.pyplot()
+
     elif choice in ["Testing Information"]:
-        st.markdown("## "+chart_dict[choice][0])
+        st.header(chart_dict[choice][0])
         df[df['BCLabTests'].notnull()]['BCLabTests'].loc[start_date : end_date + timedelta(days=1)].plot(kind='area', label="Total Tests", title = "Cumulative " + choice)
         df[df['BCTestNegative'].notnull()]['BCTestNegative'].loc[start_date : end_date + timedelta(days=1)].plot(kind='area', label='Negative Tests')
         df[df['BCTestPositive'].notnull()]['BCTestPositive'].loc[start_date : end_date + timedelta(days=1)].plot(kind='area', label='Positive Tests')
@@ -101,14 +116,14 @@ for choice in choices:
         st.pyplot()
 
     elif choice in ["Ventilator Availability", "Staffed Bed Availability"]:
-        st.markdown("## "+chart_dict[choice][0])
+        st.header(chart_dict[choice][0])
         df[chart_dict[choice][1]].loc[start_date : end_date + timedelta(days=1)].plot(kind='area', label="Total "+chart_dict[choice][3])
         df[chart_dict[choice][2]].loc[start_date : end_date + timedelta(days=1)].plot(kind='area', label='Available '+chart_dict[choice][3])
         plt.legend()
         st.pyplot()
 
     elif choice in ["Quarantine"]:
-        st.markdown("## "+chart_dict[choice][0])
+        st.header(chart_dict[choice][0])
         df[df[chart_dict[choice][1]].notnull()][chart_dict[choice][1]].loc[start_date : end_date + timedelta(days=1)].plot(title = "Cumulative " + choice)
         st.pyplot()
         df[df[chart_dict[choice][2]].notnull()][chart_dict[choice][2]].loc[start_date : end_date + timedelta(days=1)].plot(title = "Weekly " + choice, c='r')
